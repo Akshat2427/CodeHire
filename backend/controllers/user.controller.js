2a4aacb8f3a871b17f6e05a70cb32c2bca786738
Ankur2005 <ankurbhatia938@gmail.com>
2025-02-26 18:10:41 +0530

    Done making docs of routes

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,9 +2,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-
+const { validationResult } = require('express-validator');
 
 module.exports.userRegister = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
     const { email, password, name, role } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     try {
@@ -16,7 +20,8 @@ module.exports.userRegister = async (req, res) => {
                 role: role ? role : 'STUDENT',
             },
         });
-        res.status(201).json(user);
+        const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.status(201).json({user, token});
     } catch (error) {
         res.status(400).json({ error: 'User already exists' });
     }
@@ -32,6 +37,6 @@ module.exports.userLogin = async (req, res) => {
     if (!isPasswordValid) {
         return res.status(400).json({ error: 'Invalid email or password' });
     }
-    const token = jwt.sign({ userId: user.id , role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const {body} = require('express-validator');
 
-router.post('/register', require('../controllers/user.controller').userRegister);
+router.post('/register',[
+    body('name').isLength({min: 3}).withMessage('Name must be at least 3 characters long'),
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long')
+], require('../controllers/user.controller').userRegister);
 
-router.post('/login', require('../controllers/user.controller').userLogin);
+router.post('/login',[
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long')
+], require('../controllers/user.controller').userLogin);
 
 module.exports = router;
