13d3ca8fae0ac5775635e4a829cda72f2c6e1fb6
Ankur2005 <ankurbhatia938@gmail.com>
2025-03-04 23:35:28 +0530

    testing seed.js

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -4,15 +4,15 @@ const bcrypt = require('bcryptjs');
 const prisma = new PrismaClient();
 
 async function main() {
-  const hashedPassword1 = await bcrypt.hash('', 10);
-  const hashedPassword2 = await bcrypt.hash('', 10);
-  const hashedPassword3 = await bcrypt.hash('', 10);
+  const hashedPassword1 = await bcrypt.hash('Akshat@123', 10);
+  const hashedPassword2 = await bcrypt.hash('Ankur@123', 10);
+  const hashedPassword3 = await bcrypt.hash('Anshul@123', 10);
 
   await prisma.user.createMany({
     data: [
-      { name: 'Akshat', email: '', password: hashedPassword1, role: 'ADMIN' },
-      { name: 'Ankur', email: '', password: hashedPassword2, role: 'ADMIN' },
-      { name: 'Anshul', email: '', password: hashedPassword3, role: 'ADMIN' },
+      { name: 'Akshat', email: 'akshat@gmail.com', password: hashedPassword1, role: 'ADMIN' },
+      { name: 'Ankur', email: 'ankur@gmail.com', password: hashedPassword2, role: 'ADMIN' },
+      { name: 'Anshul', email: 'anshul@gmail.com', password: hashedPassword3, role: 'ADMIN' },
     ],
     skipDuplicates: true, 
   });
